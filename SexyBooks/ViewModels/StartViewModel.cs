9c8ad0d29e5f50254fe707624b6a06ff64395028
Klaus Adler <patrik.neefe@gmx.de>
2018-11-16 09:28:07 +0100

    SexyBooks

---
+++ SexyBooks/ViewModels/StartViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SexyBooks.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SexyBooks.ViewModels
+{
+    public class StartViewModel : ModelBase
+    {
+        //Eigenschaften
+        private string suchbegriff;
+
+        public string Suchbegriff
+        {
+            get { return suchbegriff; }
+            set { SetValue(ref suchbegriff, value); }
+        }
+
+    }
+}
